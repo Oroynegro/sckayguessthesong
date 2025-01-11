5ef629a2c79ccea474d69df8abc639c7a822a6ce
oroynegrothestrongest <oroynegrothestrongest@gmail.com>
2025-01-11 18:05:25 +0000

    as

--- main.js
+++ main.js
@@ -242,7 +242,7 @@ function updatePlayer(trackId) {
             width="100%"
             height="100px"
             frameBorder="0"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
         ></iframe>
     `;
